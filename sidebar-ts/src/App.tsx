f12c7fc1053f2e273d32ba3a3ada1c5c4fc622da
Victiz-R <victorraniere.trabalho@gmail.com>
2025-09-17 22:00:47 -0300

    SideBar finalizada

--- sidebar-ts/src/App.tsx
+++ sidebar-ts/src/App.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Sidebar from './components/Sidebar';
+
+
 const App: React.FC = () => {
-
+ const [collapsed, setCollapsed] = useState(false);
   return (
-      <div>
-
+    <Router>
+      <div className='d-flex'>
+        <Sidebar collapsed={collapsed} toggleSidebar={() => setCollapsed(!collapsed)} />
+        <div className='content p-4' style={{ flex: 1}}>
+          <Routes>
+            <Route path='/' element={<h1>Home</h1>}/>
+            <Route path='/Redes' element={<h1>Redes Sociais</h1>}/>
+            <Route path='/Contatos' element={<h1>Contatos</h1>}/>
+          </Routes>
+        </div>
       </div>
-
+    </Router>
   )
 }
 
--- sidebar-ts/src/components/Sidebar.tsx
+++ sidebar-ts/src/components/Sidebar.tsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { FaHome, FaInfoCircle, FaPhone, FaBars} from 'react-icons/fa';
+import { Button, Nav } from "react-bootstrap";
+import "./Sidebar.css";
 
-const Sidebar = () => {
+interface SidebarProps{
+  collapsed: boolean;
+  toggleSidebar: () => void;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ collapsed, toggleSidebar }) => {
+  const location = useLocation();
+
+  const menuItens = [
+    { name: "Home", path: "/", icon: <FaHome />},
+    { name: "Redes Sociais", path: "/Redes", icon: <FaInfoCircle/> },
+    { name: "Contatos", path: "/Contato", icon: <FaPhone />},
+  ];
   return (
-    <div>Sidebar</div>
+    <div className={`sidebar ${collapsed ? "collapsed": ""}`} >
+      <Button variant="primary" onClick={toggleSidebar} className='mb-3 toggle-btn'>
+        <FaBars />
+      </Button>
+
+      <Nav className='flex-column'>
+        {menuItens.map((item) =>(
+          <Nav.Item key={item.path} className={location.pathname === item.path ? "active": ""}>
+            <Link to={item.path} className='nav-link d-flex align-items-center'>
+            <span className='icon me-2'>{item.icon}</span>
+            {!collapsed && <span className="label">{item.name}</span>}
+            </Link>
+          </Nav.Item>
+
+        ))}
+      </Nav>
+    </div>
   )
 }
 
