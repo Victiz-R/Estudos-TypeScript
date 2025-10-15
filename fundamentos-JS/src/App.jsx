8a32c4d2bfa3625531a5022f9e9a4b05284a1f3d
Victiz-R <victorraniere.trabalho@gmail.com>
2025-10-15 01:09:34 -0300

    Atualizando estudos sobre JavaScript(Hooks)

---
+++ fundamentos-JS/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Welcome from './components/Welcome'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+function App() {
+
+
+  return (
+    <>
+     <div class="bg-primary text-white w-100 d-flex justify-content-center align-items-center">
+  Welcome
+</div>
+
+    </>
+  )
+}
+
+export default App
